ac80ae3c41bf13b62a3969d1574b6120caf24f2c
Vasu Dev Mishra <vasudevmishra04@gmail.com>
2023-10-20 08:17:05 +0530

    Data Preprocessing

--- customer_churn_prediction_script.py
+++ customer_churn_prediction_script.py
@@ -1,7 +1,16 @@
 import pandas as pd
-
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 # Load the dataset
 churn_data = pd.read_excel("./customer_churn_large_dataset.xlsx")
 
-print(churn_data.head())
+churn_data.dropna(subset=["Gender", "Location"], inplace=True)
+for column in ["Gender", "Location"]:
+    le = LabelEncoder()
+    churn_data[column] = le.fit_transform(churn_data[column])
+
+# Split the data into training and testing sets
+X = churn_data.drop(columns=["CustomerID", "Name", "Churn"])
+y = churn_data["Churn"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
